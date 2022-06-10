f2d6d536c90a59ba17ad0f39f59bf62db1837b89
Black_block <johnsonlee911205@gmail.com>
2022-06-10 17:10:14 +0800

    QT SETUP

---
+++ build-QT_Equatio_drawing-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -0,0 +1,9 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/QT/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "D:/QT/6.2.4/mingw_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "D:/QT/6.2.4/mingw_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "D:/QT/Tools/mingw1120_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "D:/QT/Tools/mingw1120_64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
